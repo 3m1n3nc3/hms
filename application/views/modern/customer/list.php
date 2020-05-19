60a243e1d8d8456f44567be43f985f418803db8d
3m1n3nc3 <mygames.ng@gmail.com>
2020-05-20 00:56:57 +0100

    Bug fixes

--- application/views/modern/customer/list.php
+++ application/views/modern/customer/list.php
@@ -32,32 +32,33 @@
                   </thead>
                   <tbody>
                     <?php if ($customers): ?>
-                    <?php foreach ($customers as $customer): ?>
-                    <tr>
-                      <td>
-                        <a href="<?= $customer->customer_id !== '0' ? site_url('customer/data/'.$customer->customer_TCno) : 'javascript:void(0)' ?>">
-                          <?=$customer->customer_firstname . ' ' . $customer->customer_lastname;?>
-                        </a>
-                      </td>
-                      <td> <?=$customer->customer_telephone;?> </td>
-                      <td> <?=$customer->customer_email;?> </td>
-                      <td> <?=$customer->customer_TCno;?> </td>
-                      <td> <?=$this->cr_symbol.number_format($customer->debt, 2)?> </td>
-                      <td class="td-actions">
-                        <?php if ($customer->customer_id !== '0'): ?>
-                        <a href="<?= site_url('customer/reserve/'.$customer->customer_TCno) ?>" class="btn btn-sm btn-success" data-toggle="tooltip" title="Reserve">
-                          <i class="btn-icon-only fa fa-calendar-check text-white"></i>
-                        </a>
-                        <a href="<?= site_url('customer/add/'.$customer->customer_id.'/update') ?>" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Edit">
-                          <i class="btn-icon-only fa fa-edit text-white"></i>
-                        </a>
-                        <a href="javascript:void(0)" onclick="return confirmDelete('<?= site_url('customer/delete/'.$customer->customer_id) ?>', 1)" class="btn btn-danger btn-sm" data-toggle="tooltip" title="Delete">
-                          <i class="btn-icon-only fa fa-trash text-white"></i>
-                        </a>
-                        <?php endif; ?>
-                      </td>
-                    </tr>
-                    <?php endforeach; ?>
+                      <?php foreach ($customers as $customer): ?>
+                      <?php $stats = $this->accounting_model->statistics(['customer' => $customer['customer_id']]); ?>
+                      <tr>
+                        <td>
+                          <a href="<?= $customer->customer_id !== '0' ? site_url('customer/data/'.$customer->customer_TCno) : 'javascript:void(0)' ?>">
+                            <?=$customer->name;?>
+                          </a>
+                        </td>
+                        <td> <?=$customer->customer_telephone;?> </td>
+                        <td> <?=$customer->customer_email;?> </td>
+                        <td> <?=$customer->customer_TCno;?> </td>
+                        <td> <?=$this->cr_symbol.number_format($stats['debt'], 2)?> </td>
+                        <td class="td-actions">
+                          <?php if ($customer->customer_id !== '0'): ?>
+                          <a href="<?= site_url('customer/reserve/'.$customer->customer_TCno) ?>" class="btn btn-sm btn-success" data-toggle="tooltip" title="Reserve">
+                            <i class="btn-icon-only fa fa-calendar-check text-white"></i>
+                          </a>
+                          <a href="<?= site_url('customer/add/'.$customer->customer_id.'/update') ?>" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Edit">
+                            <i class="btn-icon-only fa fa-edit text-white"></i>
+                          </a>
+                          <a href="javascript:void(0)" onclick="return confirmDelete('<?= site_url('customer/delete/'.$customer->customer_id) ?>', 1)" class="btn btn-danger btn-sm" data-toggle="tooltip" title="Delete">
+                            <i class="btn-icon-only fa fa-trash text-white"></i>
+                          </a>
+                          <?php endif; ?>
+                        </td>
+                      </tr>
+                      <?php endforeach; ?>
                     <?php else: ?>
                     <tr>
                       <td colspan="5" class="text-center"><?php alert_notice('No rooms available', 'info', TRUE, FALSE) ?></td>
